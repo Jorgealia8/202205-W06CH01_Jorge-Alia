8c795b9f090ca15b5df8e7bdabf6a2b86a6e005c
Jorge Alia <jorgealia94@gmail.com>
2022-06-13 18:37:05 +0200

    Add reducer and interfaz character

---
+++ src/reducers/action.creators.ts
@@ -0,0 +1,24 @@
+import { createAction } from '@reduxjs/toolkit';
+import { CharacterModel } from '../models/character';
+import { actionTypes } from './action.types';
+
+export interface iAction {
+  type: actionTypes;
+  payload?: any;
+}
+
+export const loadCharacterAction = createAction<Array<CharacterModel>>(
+  actionTypes['character@load'].toLocaleString()
+);
+
+export const addCharacterAction = createAction<CharacterModel>(
+  actionTypes['character@add'].toLocaleString()
+);
+
+export const updateCharacterAction = createAction<CharacterModel>(
+  actionTypes['character@update'].toLocaleString()
+);
+
+export const deleteCharacterAction = createAction<CharacterModel>(
+  actionTypes['character@delete'].toLocaleString()
+);
